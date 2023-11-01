e14953af104689ff06766e941f44ea38fe039569
Jacob Brandauer <jacobbrandauer@n190-p214.eduroam.kth.se>
2023-11-01 12:24:45 +0100

    Modifications for wide-res-net and resnet implementation

--- resNet.py
+++ resNet.py
@@ -1,7 +1,8 @@
-"""Wide residual network (WRN) implementation.
-
-The implementation follows the WRN as described by Zagoruyko and Komodakis (2017).
 """
+    ResNet implementation
+The implementation follows the ResNet as described by He et al. (2015). with modifications described by Mukhoti et al. (2023)
+"""
+
 import tensorflow as tf
 from tensorflow import pad
 from tensorflow.keras import Input, Model
@@ -12,19 +13,8 @@ from tensorflow.keras.regularizers import l2
 import tensorflow_addons as tfa
 
 
-class spectral_norm(tf.keras.layers.Wrapper):
-    def __init__(self, layer):
-        super(spectral_norm, self).__init__(layer)
-        self.layer = tfa.layers.SpectralNormalization(layer)
-    @tf.function
-    def call(self, inputs,training=None):
-        return self.layer(inputs)
-        
-    
 
-
-
-def WRN(N, k, in_shape, n_out, dropout=0, weight_decay=1e-4, modBlock=True):
+def resnet(stages, N, in_filters, in_shape, n_out, dropout=0, weight_decay=1e-4, modBlock=True, use_bottleneck=False):
     """WRN-n-k as described by Zagoruyko and Komodakis (2017).
 
     This network has n=7N layers (2N for each of the three stages with an additional convolution at the start and at
@@ -33,23 +23,25 @@ def WRN(N, k, in_shape, n_out, dropout=0, weight_decay=1e-4, modBlock=True):
     Note that:
     - WRN-28-10 has N=4 and k=10.
 
+    :param stages: list of number of filters
     :param N: Number of blocks per stage.
+    :param in_filters: Number of filters for input convolution
     :param k: Widening factor.
     :param in_shape: Input shape.
     :param n_out: Output size.
     :param dropout: Dropout rate.
     :param weight_decay: Weight decay parameter.
     """
-
-    stages = [16 * k, 32 * k, 64 * k]
+    
     inputs = Input(shape=in_shape)
+
 
     # First convolution
     if(modBlock):
-        x = SpectralNormalization(Conv2D(16, 3, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
+        x = SpectralNormalization(Conv2D(in_filters, 3, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
                kernel_regularizer=l2(weight_decay)))(inputs)
     else: 
-        x = Conv2D(16, 3, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
+        x = Conv2D(in_filters, 3, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
                kernel_regularizer=l2(weight_decay))(inputs)
     
 
@@ -58,10 +50,15 @@ def WRN(N, k, in_shape, n_out, dropout=0, weight_decay=1e-4, modBlock=True):
         n_filters = stages[i]
         for j in range(N):
             if i > 0 and j == 0:
-                x = block(x, n_filters, dropout, weight_decay, downsample=True, modBlock=modBlock)
+                if(not use_bottleneck):
+                    x = block(x, n_filters, dropout, weight_decay, downsample=True, modBlock=modBlock)
+                else: 
+                    x = bottleneck_block(x, n_filters, dropout, weight_decay, downsample=True, modBlock=modBlock)
             else:
-                x = block(x, n_filters, dropout, weight_decay, modBlock=modBlock)
-
+                if(not use_bottleneck):
+                    x = block(x, n_filters, dropout, weight_decay, modBlock=modBlock)
+                else:
+                    x = bottleneck_block(x, n_filters, dropout, weight_decay, downsample=True, modBlock=modBlock)
     # Pooling and dense output layer with softmax activation.
     x = BatchNormalization()(x)
     x = relu(x)
@@ -104,10 +101,7 @@ def block(x, n_filters, dropout, weight_decay, downsample=False, modBlock = True
         start_stride = 2
     else:
         start_stride = 1
-
     x_skip = x
-
-
     x = BatchNormalization()(x)
     x = activation(x)
     if(modBlock):
@@ -170,5 +164,121 @@ def block(x, n_filters, dropout, weight_decay, downsample=False, modBlock = True
     x = Add()([x_skip, x])
 
     return x
+
+
+def bottleneck_block(x, n_filters, dropout, weight_decay, downsample=False, modBlock = True):
+    """Basic ResNet-bottleneck-block.
+
+    :param x: Input.
+    :param n_filters: Number of filters.
+    :param dropout: Dropout rate.
+    :param weight_decay: Weight decay parameter.
+    :param downsample: True if the layer should downsample.
+    """
+
+    if(modBlock): 
+        activation = tf.keras.layers.LeakyReLU()
+    else: 
+        activation = tf.keras.layers.ReLU()
+
+
+    if downsample:
+        start_stride = 2
+    else:
+        start_stride = 1
+    x_skip = x
+
+    x = BatchNormalization()(x)
+    x = activation(x)
+    if(modBlock):
+        x = SpectralNormalization(Conv2D(n_filters, 1, start_stride, padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay)))(x)
+    else: 
+        x = Conv2D(n_filters, 1, start_stride, padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay))(x)
+
+
+    x = BatchNormalization()(x)
+    x = activation(x)
+
+    if(modBlock):
+        x = SpectralNormalization(Conv2D(n_filters, 3, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay)))(x)
+    else: 
+        x = Conv2D(n_filters, 3, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay))(x)
+
+    x = BatchNormalization()(x)
+    x = activation(x)
+    if(dropout > 0):
+        x = Dropout(dropout)(x)
+    
+    if(modBlock):
+        x = SpectralNormalization(Conv2D(n_filters, 3, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay)))(x)
+    else:
+        x = Conv2D(n_filters, 3, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay))(x)
+        
+
+    x = BatchNormalization()(x)
+    x = activation(x)
+
+    if(modBlock):
+        x = SpectralNormalization(Conv2D(4*n_filters, 1, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay)))(x)
+    else:
+        x = Conv2D(4*n_filters, 1, 1, padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay))(x)
+        
+    
+    
+    print("Shape x: ", tf.shape(x))
+    print("N filters: ", n_filters)
+    
+
+    if downsample:
+        if(not modBlock):
+            x_skip = Conv2D(4*n_filters, 1, 2, use_bias=False, kernel_initializer='he_normal',
+                        kernel_regularizer=l2(weight_decay))(x_skip)
+        else: 
+            # change to standard ResNet: Authors use strided average pooling instead of a 1x1-conv. layer
+            # see: Appedix C.1 - "Deep Deterministic Uncertainty: A New Simple Baseline", Mukhoti et al. (2023)
+            # x_skip = AveragePooling2D(pool_size =  (start_stride, start_stride), strides=start_stride)(x)
+
+            # # add padded zero entries if number of channels increases
+            # if(n_filters > x_skip.shape[-1]):
+            #     zero_entries = tf.zeros(shape=[x_skip.shape[0], x_skip.shape[1], x_skip.shape[2], n_filters-x_skip.shape[3]])
+
+            #     # concatenate in dimension of channels
+            #     x_skip = tf.concat([x_skip, zero_entries], axis=-1)
+
+            if(x_skip.shape[1] % 2 == 0):
+                print("Downsample")
+                x_skip = AveragePooling2D(pool_size =  2, strides=2)(x_skip)
+            else:
+                #just 1x1 average pooling in that case, could be replaced with usual 1x1-convolutions
+                x_skip = AveragePooling2D(pool_size = 1, strides=2)(x_skip)
+            # add padded zero entries if number of channels increases
+            if(4*n_filters > x_skip.shape[-1]):
+                # zero_entries = tf.zeros(shape=[x_skip.shape[0], x_skip.shape[1], x_skip.shape[2], n_filters-x_skip.shape[3]])
+                # concatenate in dimension of channels
+                missing = 4*n_filters-x_skip.shape[3]
+                x_skip = tf.pad(x_skip, [[0,0], [0,0], [0,0], [missing //2, -(missing // -2)]])
+                print("Shape x_skip: ", tf.shape(x_skip))
+    elif(n_filters > x_skip.shape[-1]):
+        print("INcrease depth")
+        missing = n_filters-x_skip.shape[3]
+        x_skip = tf.pad(x_skip, [[0,0], [0,0], [0,0], [missing //2, -(missing // -2)]])
+
+    x = Add()([x_skip, x])
+
+    return x
+
+        
+    
+
+
+
 
 
---
+++ test_wrn.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from WRN import WRN
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def normalize(image, label):
+    return tf.cast(image, tf.float32)/255., label
+
+if __name__ == '__main__':
+
+    # (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    ds_train = tfds.load('mnist', split='train')
+    ds_test = tfds.load('mnist', split='test')
+
+    # test on ResNet18 architecture
+    wideResNet = WRN(N=4, in_shape=(28,28,1), k=10, n_out=10, modBlock=False) 
+
+    loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+    metric = tf.keras.metrics.SparseCategoricalAccuracy()
+
+    
+    # construct training and test data
+    trainX = np.zeros((60000, 28,28,1), dtype=np.float32)
+    trainY = np.zeros((60000,), dtype=np.int32)
+    testX = np.zeros((10000, 28, 28,1), dtype=np.float32)
+    testY = np.zeros((10000,), dtype=np.int32)
+    for i, elem in enumerate(ds_train):
+        # print(elem)
+        trainX[i, :, :, :] = tf.cast(elem['image'], tf.float32)/255.
+        trainY[i] = elem['label']
+    for i, elem in enumerate(ds_test):
+        # print(elem)
+        testX[i, :, :, :] = tf.cast(elem['image'], tf.float32)/255.
+        testY[i] = elem['label']
+    
+    # show image
+    image = trainX[0]
+    plt.imshow(image)
+    plt.show()
+
+    # test model on single image - expand batch dim first
+    image = tf.expand_dims(image, axis=0)
+    out = wideResNet(image)
+    print("Output: ", out)
+
+    wideResNet.summary()
+
+    wideResNet.fit(x=trainX, y=trainY,batch_size=128,epochs=10,validation_data=(testX, testY))
+
