de6f7225fcf6b97472c925ecec049c65ecf65476
stefanvanberkum <40003032+stefanvanberkum@users.noreply.github.com>
2023-11-06 16:06:28 +0100

    Fixed modblock argument.

--- multi_run.sh
+++ multi_run.sh
@@ -3,26 +3,26 @@
 # Five runs on CIFAR-10 and CIFAR-100 for one model.
 
 # WRN models:
-# - WRN softmax: model_type=wrn, modblock=0, ablate=0
-# - WRN DDU: model_type=wrn, modblock=1, ablate=0
-# - WRN ensemble: model_type=wrn-ensemble, modblock=0, ablate=0
-# - WRN DDU ablation: model_type=wrn, modblock=1, ablate=1
+# - WRN softmax: model_type=wrn, modBlock=False, ablate=False
+# - WRN DDU: model_type=wrn, modBlock=True, ablate=False
+# - WRN ensemble: model_type=wrn-ensemble, modBlock=False, ablate=False
+# - WRN DDU ablation: model_type=wrn, modBlock=True, ablate=True
 
 # ResNet models:
-# - ResNet softmax: model_type=resnet, modblock=0, ablate=0
-# - ResNet DDU: model_type=resnet, modblock=1, ablate=0
-# - ResNet ensemble: model_type=resnet-ensemble, modblock=0, ablate=0
-# - ResNet DDU ablation: model_type=resnet, modblock=1, ablate=1
+# - ResNet softmax: model_type=resnet, modBlock=False, ablate=False
+# - ResNet DDU: model_type=resnet, modBlock=True, ablate=False
+# - ResNet ensemble: model_type=resnet-ensemble, modBlock=False, ablate=False
+# - ResNet DDU ablation: model_type=resnet, modBlock=True, ablate=True
 
 model_type=wrn
-modblock=1
-ablate=0
+modBlock=True
+ablate=False
 
 for run in {1..5}
 do
 	for dataset in cifar10 cifar100
 	do
-		python train_models.py --model $model_type --dataset $dataset --modblock $modblock --ablate $ablate --n_run $run
+		python train_models.py --model $model_type --dataset $dataset --modBlock $modBlock --ablate $ablate --n_run $run
 		wait
 	done
 done
